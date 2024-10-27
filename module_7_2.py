9569f36317c929acdcc4632fd5e2428f321e62af
nataliafatkina <fatkina.natali@gmail.com>
2024-10-27 15:14:29 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    number_str = 0
+
+    file = open(file_name, 'a', encoding='utf-8')
+
+    for string in strings:
+        number_str += 1
+        start_str = file.tell()
+        file.write(string + '\n')
+        strings_positions.update({(number_str, start_str): string})
+    file.close()
+    return print(strings_positions)
+
+
+info = ['Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!']
+
+custom_write('test.txt', info)
